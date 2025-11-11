a7e070e9dc36876fb5c50c7dc90de6668aabdd99
studyweb <studyweb.help@gmail.com>
2025-11-11 16:57:37 +0000

    Try fixing this error: `Runtime Error: Error: Invalid supabaseUrl: Must

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 // The ! after the environment variable asserts that it's non-null.
 // This is safe because the app will fail to build if these are not set.
 export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    'https://your-project-id.supabase.co',
+    'your-anon-key'
 );
